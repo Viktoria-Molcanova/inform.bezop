db5e92168c4fb996f49bc76a16c280fff9a319f3
Viktoria-Molcanova <119111084+Viktoria-Molcanova@users.noreply.github.com>
2024-11-11 20:29:46 +0700

    Update 1.py

--- seminar10/1.py
+++ seminar10/1.py
@@ -1,1 +1,29 @@
+import re
+import hashlib
 
+def check_password_complexity(password):
+    if len(password) < 8:
+        return False
+    if not re.search("[a-z]", password):
+        return False
+    if not re.search("[A-Z]", password):
+        return False
+    if not re.search("[0-9]", password):
+        return False
+    return True
+
+def hash_password(password):
+    return hashlib.sha256(password.encode()).hexdigest()
+
+def main():
+    password = input("Введите пароль: ")
+
+    if check_password_complexity(password):
+        hashed_password = hash_password(password)
+        print("Пароль соответствует требованиям сложности.")
+        print("Хэш пароля:", hashed_password)
+    else:
+        print("Пароль не соответствует требованиям сложности.")
+
+if __name__ == "__main__":
+    main()
